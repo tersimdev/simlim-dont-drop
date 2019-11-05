01f39e5aa0ce930713835b6f17f52e0def70b0e3
InTheAxis <codeaxex@gmail.com>
2019-11-05 10:47:40 +0800

    added main menu and splash screen

--- app/src/main/java/com/simlim/mobileMod/MainMenu.java
+++ app/src/main/java/com/simlim/mobileMod/MainMenu.java
@@ -1,6 +1,59 @@
 package com.simlim.mobileMod;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainMenu extends Activity {
+public class MainMenu extends Activity implements View.OnClickListener {
+
+    private Button btn_start, btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.main_menu);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_start.setOnClickListener(this);
+        btn_back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+
+        if (v == btn_start) {
+            intent.setClass(this, SplashPage.class);
+        }
+        else if (v == btn_back) {
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+        }
+
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
--- app/src/main/java/com/simlim/mobileMod/SplashPage.java
+++ app/src/main/java/com/simlim/mobileMod/SplashPage.java
@@ -1,6 +1,76 @@
 package com.simlim.mobileMod;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashPage extends Activity {
+
+    protected boolean active = true;
+    protected int splashTime = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_page);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+
+                    int waited = 0;
+                    while (active && waited < splashTime) {
+                        sleep(200);
+                        if (active) {
+                            waited += 200;
+                        }
+                    }
+                }
+                catch (InterruptedException e) {
+
+                }
+                finally {
+                    finish();
+                    Intent i = new Intent(SplashPage.this, MainMenu.class);
+                    startActivity(i);
+
+
+                }
+            }
+        };
+
+        splashThread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            active = false;
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
