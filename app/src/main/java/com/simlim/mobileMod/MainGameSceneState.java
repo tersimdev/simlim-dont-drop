4a56183c9082163cd8e0a6d2a20a6a57e5dc147d
lim-james <jameslimbj@gmail.com>
2019-11-21 14:41:41 +0800

    aded selection to map

--- app/src/main/java/com/simlim/mobileMod/MainGameSceneState.java
+++ app/src/main/java/com/simlim/mobileMod/MainGameSceneState.java
@@ -1,11 +1,9 @@
 package com.simlim.mobileMod;
 
-import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Rect;
-import android.text.method.Touch;
 import android.view.SurfaceView;
 
 import java.time.Clock;
@@ -17,11 +15,12 @@ import java.util.List;
 public class MainGameSceneState implements StateBase {
     private float timer = 0.0f;
 
-    private int mapWidth = 8;
-    private int mapHeight = 12;
+    private int mapWidth = 24;
+    private int mapHeight = 24;
     private float mapOffsetX = 0.f;
     private float mapOffsetY = 0.f;
     private float tileSize = 0.f;
+    private float padding = 0.f;
 
     private List<GameObject> map = new ArrayList<GameObject>();
 
@@ -41,6 +40,8 @@ public class MainGameSceneState implements StateBase {
         // for Pause Button
 
         // Generating map
+        if (map.size() != 0) return;
+
         final float mapHalfWidth = (float)mapWidth * 0.5f;
         final float mapHalfHeight = (float)mapHeight * 0.5f;
 
@@ -53,19 +54,20 @@ public class MainGameSceneState implements StateBase {
 
         for (int x = 0; x < mapWidth; ++x) {
             for (int y = 0; y < mapHeight; ++y) {
-                final int posX = x * (int)tileSize + (int)mapOffsetX;
-                final int posY = y * (int)tileSize + (int)mapOffsetY;
+                final int posX = x * (int)tileSize + (int)mapOffsetX + (int)padding;
+                final int posY = y * (int)tileSize + (int)mapOffsetY + (int)padding;
+                final int size = (int)(tileSize - 2.f * padding);
 
                 GameObject tile = new GameObject();
                 tile.color = Color.RED;
-                tile.rect = new Rect(posX, posY, posX + (int)(tileSize * 0.8f), posY + (int)(tileSize * 0.8f));
+                tile.rect = new Rect(posX, posY, posX + size, posY + size);
                 EntityManager.Instance.AddEntity(tile);
                 map.add(tile);
             }
         }
 
         player = new GameObject();
-        player.color = Color.GREEN;
+        player.color = Color.BLUE;
         player.rect = new Rect(0, 0, (int)(tileSize * 0.8f), (int)(tileSize * 0.8f));
         EntityManager.Instance.AddEntity(player);
     }
@@ -85,32 +87,41 @@ public class MainGameSceneState implements StateBase {
 
     @Override
     public void Update(float _dt) {
-
         EntityManager.Instance.Update(_dt);
 
-        if (TouchManager.Instance.IsDown()) {
-			
+        if (TouchManager.Instance.HasTouch()) {
             //Example of touch on screen in the main game to trigger back to Main menu
-            float x = TouchManager.Instance.GetPosX();// - map.get(0).GetPosX();
-            float y = TouchManager.Instance.GetPosY();// - map.get(0).GetPosY();
-//            if (Math.abs(x) < 100 && Math.abs(y) < 100)
+            final float x = TouchManager.Instance.GetPosX();
+            final float y = TouchManager.Instance.GetPosY();
 
-            map.get(getScreenspaceToMapIndex(x, y)).color = Color.GREEN;
+            final int mapIndex = getScreenSpaceToMapIndex(x, y);
+            if (mapIndex >= 0)
+                map.get(mapIndex).color = Color.GREEN;
 
-//            player.SetPosX(x);
-//            player.SetPosY(y);
+            PointF mapPosition = getScreenSpaceToMapPosition(x, y);
+
+            player.SetPosX(mapPosition.x * tileSize + mapOffsetX + tileSize * 0.5f);
+            player.SetPosY(mapPosition.y * tileSize + mapOffsetY + tileSize * 0.5f);
         }
     }
 
     private int getMapIndex(int _x, int _y) {
+        if (_x < 0 || _x >= mapWidth || _y < 0 || _y >= mapHeight)
+            return -1;
+
         return _x * mapHeight + _y;
     }
 
-    private int getScreenspaceToMapIndex(float _x, float _y) {
-        final int x = (int)((_x - mapOffsetX + 0.5f) / tileSize);
-        final int y = (int)((_y - mapOffsetY + 0.5f) / tileSize);
+    private int getScreenSpaceToMapIndex(float _x, float _y) {
+        final PointF position = getScreenSpaceToMapPosition(_x, _y);
+        return getMapIndex((int)position.x, (int)position.y);
+    }
 
-        return getMapIndex(x, y);
+    private PointF getScreenSpaceToMapPosition(float _x, float _y) {
+        return new PointF(
+                (float)Math.floor((_x - mapOffsetX) / tileSize),
+                (float) Math.floor((_y - mapOffsetY) / tileSize)
+        );
     }
 
 }
