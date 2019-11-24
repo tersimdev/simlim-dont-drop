d77746a23e04cf30c0387100a622e12927a3b597
lim-james <jameslimbj@gmail.com>
2019-11-24 22:55:54 +0800

    basic map path generation

--- app/src/main/java/com/simlim/mobileMod/MainGameSceneState.java
+++ app/src/main/java/com/simlim/mobileMod/MainGameSceneState.java
@@ -2,8 +2,10 @@ package com.simlim.mobileMod;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.service.quicksettings.Tile;
 import android.view.SurfaceView;
 
 import java.time.Clock;
@@ -57,10 +59,26 @@ public class MainGameSceneState implements StateBase {
         // for Pause Button
 
         // Generating map
-        if (mapObjects.size() != 0) return;
+        if (mapObjects.size() != 0) {
+            for (int i = 0; i < mapWidth * mapHeight; ++i) {
+                map.set(i, TileType.BLOCKED);
+            }
 
-        final float mapHalfWidth = (float)mapWidth * 0.5f;
-        final float mapHalfHeight = (float)mapHeight * 0.5f;
+            PointF direction = new PointF();
+            direction.x = 0;
+            direction.y = 1;
+
+            map.set(0, TileType.PATH);
+
+            generatePath(map, new PointF(), direction, 0, 10);
+            return;
+        }
+
+        for (int x = 0; x < mapWidth; ++x) {
+            for (int y = 0; y < mapHeight; ++y) {
+                map.add(TileType.BLOCKED);
+            }
+        }
 
         final float mapScreenWidth = (float)_view.getHeight() * 0.5f;
         tileSize = mapScreenWidth / (float)mapWidth;
@@ -69,10 +87,17 @@ public class MainGameSceneState implements StateBase {
         mapOffsetX =  ((float)_view.getWidth() - mapScreenWidth) * 0.5f;
         mapOffsetY =  ((float)_view.getHeight() - mapScreenHeight) * 0.5f;
 
+        PointF direction = new PointF();
+        direction.x = 0;
+        direction.y = 1;
+
+        map.set(0, TileType.PATH);
+
+        generatePath(map, new PointF(), direction, 0, 10);
+
         for (int x = 0; x < mapWidth; ++x) {
             for (int y = 0; y < mapHeight; ++y) {
-                final TileType type = TileType.random();
-                map.add(type);
+                TileType type = map.get(getMapIndex(x, y));
 
                 final int posX = x * (int)tileSize + (int)mapOffsetX + (int)padding;
                 final int posY = y * (int)tileSize + (int)mapOffsetY + (int)padding;
@@ -103,7 +128,7 @@ public class MainGameSceneState implements StateBase {
                 mapObjects.add(tile);
             }
         }
-        
+
         player = new GameObject();
         player.color = Color.BLUE;
         player.rect = new Rect(0, 0, (int)(tileSize * 0.8f), (int)(tileSize * 0.8f));
@@ -138,9 +163,80 @@ public class MainGameSceneState implements StateBase {
 
             PointF mapPosition = getScreenSpaceToMapPosition(x, y);
 
-            player.SetPosX(mapPosition.x * tileSize + mapOffsetX + tileSize * 0.5f);
-            player.SetPosY(mapPosition.y * tileSize + mapOffsetY + tileSize * 0.5f);
+            player.SetPosX(mapPosition.x * tileSize + mapOffsetX);
+            player.SetPosY(mapPosition.y * tileSize + mapOffsetY);
         }
+    }
+
+    private void generatePath(List<TileType> mapRef, PointF position, PointF direction, int depth, int min) {
+        System.out.println("Generate");
+
+        Random random = new Random();
+
+        ++depth;
+
+        if (random.nextBoolean() || depth < min) {
+            final PointF newPosition = setPath(mapRef, position, direction);
+
+            if (newPosition.x != -1.f) {
+                generatePath(mapRef, newPosition, direction, depth, min);
+            }
+        }
+
+        if (random.nextBoolean()) {
+            PointF newDirection = new PointF();
+            newDirection.x = direction.y;
+            newDirection.y = direction.x;
+
+            final PointF newPosition = setPath(mapRef, position, newDirection);
+
+            if (newPosition.x != -1.f) {
+                generatePath(mapRef, newPosition, newDirection, depth, min);
+            }
+        }
+
+        if (random.nextBoolean()) {
+            PointF newDirection = new PointF();
+            newDirection.x = -direction.y;
+            newDirection.y = -direction.x;
+
+            final PointF newPosition = setPath(mapRef, position, newDirection);
+
+            if (newPosition.x != -1.f) {
+                generatePath(mapRef, newPosition, newDirection, depth, min);
+            }
+        }
+    }
+
+    private PointF setPath(List<TileType> mapRef, PointF position, PointF direction) {
+        PointF newPosition = new PointF();
+        newPosition.x = position.x;
+        newPosition.y = position.y;
+
+        for (int i = 0; i < 5; ++i) {
+            newPosition = add(newPosition, direction);
+            int index = getMapIndex(newPosition);
+            if (index >= 0 && mapRef.get(index) == TileType.BLOCKED) {
+                if (new Random().nextInt(10) == 0) {
+                    mapRef.set(index, TileType.TRASH);
+                } else {
+                    mapRef.set(index, TileType.PATH);
+                }
+            } else {
+                newPosition.x = -1.f;
+                newPosition.y = -1.f;
+                break;
+            }
+        }
+
+        return newPosition;
+    }
+
+    private PointF add(PointF a, PointF b) {
+        PointF p = new PointF();
+        p.x = a.x + b.x;
+        p.y = a.y + b.y;
+        return p;
     }
 
     private int getMapIndex(int _x, int _y) {
@@ -148,6 +244,16 @@ public class MainGameSceneState implements StateBase {
             return -1;
 
         return _x * mapHeight + _y;
+    }
+
+    private int getMapIndex(PointF _p) {
+        final int x = (int)_p.x;
+        final int y = (int)_p.y;
+
+        if (x < 0 || x >= mapWidth || y < 0 || y >= mapHeight)
+            return -1;
+
+        return x * mapHeight + y;
     }
 
     private int getScreenSpaceToMapIndex(float _x, float _y) {
