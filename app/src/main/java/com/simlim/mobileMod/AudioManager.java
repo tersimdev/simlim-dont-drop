63f399f3502955e7b4524872110a54d00fee800a
InTheAxis <codeaxex@gmail.com>
2019-12-30 00:20:52 +0800

    added audio manager

---
+++ app/src/main/java/com/simlim/mobileMod/AudioManager.java
@@ -0,0 +1,48 @@
+package com.simlim.mobileMod;
+
+import android.media.MediaPlayer;
+import android.view.SurfaceView;
+
+import java.util.HashMap;
+
+public class AudioManager {
+    public final static AudioManager Instance = new AudioManager();
+    private HashMap<Integer, MediaPlayer> audioMap = new HashMap<>();
+    private SurfaceView view = null;
+
+    private AudioManager() { }
+
+    public void Init(SurfaceView _view) {
+        view = _view;
+        Reset();
+    }
+
+    public void Reset() {
+        for (HashMap.Entry<Integer, MediaPlayer> entry : audioMap.entrySet()) {
+            MediaPlayer m = entry.getValue();
+            m.stop();
+            m.reset();
+            m.release();
+        }
+        audioMap.clear();
+    }
+
+    public void PlayAudio(final int _id, float _vol) {
+        if (audioMap.containsKey(_id)) {
+            MediaPlayer m = audioMap.get(_id);
+            m.seekTo(0);
+            m.setVolume(_vol, _vol);
+            m.start();
+        }
+        else {
+            MediaPlayer m = MediaPlayer.create(view.getContext(), _id);
+            audioMap.put(_id, m);
+            m.start();
+        }
+    }
+
+    public void StopAudio(int _id) {
+        if (audioMap.containsKey(_id))
+            audioMap.get(_id).pause();
+    }
+}
